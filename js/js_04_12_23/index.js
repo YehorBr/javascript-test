65ff24d105c2ebed6a2e24b12e80f8953fab312d
Yehor <yehor12344321@gmail.com>
2023-12-08 19:00:55 +0200

    зберігає зміни

---
+++ js/js_04_12_23/index.js
@@ -0,0 +1,61 @@
+// const elementWidth = '124px';
+
+// const parsedElementWidth = Number.parseInt(elementWidth);
+// console.log(parsedElementWidth);
+
+
+
+// const elementheight = '623px';
+
+// // const result = Number.parseFloat(elementheight);
+// // console.log(result)
+
+
+
+// const salary = 1241.41214;
+// console.log(salary);
+
+// console.log(Number(salary.toFixed(1)));
+
+
+
+// const quantity = "30";
+// const value = "Цей рядок неможливо перетворити в число";
+
+// console.log(Number.isNaN(value));
+
+
+
+// const  minNumber = Math.min(10, 20, 2, 5);
+// console.log(minNumber);
+
+// const  maxNumber = Math.max(10, 20, 2, 5);
+// console.log(maxNumber);
+
+
+// const base = 41;
+// const power = 12;
+
+// const  powNumber = Math.pow(base, power);
+// console.log(powNumber);
+
+// console.log(base ** power);
+
+// const promptBase = prompt("base number");
+// console.log("Число:", promptBase);
+// const promptPower = prompt("power number");
+// console.log("Степінь:", promptPower);
+
+// const result = Math.pow(promptBase, promptPower);
+// console.log(result);
+
+const randomNumber = Math.random() * (10 - 15) + 15;
+console.log(randomNumber);
+
+
+
+
+
+
+
+
---
+++ js/js_27_11_23/index.js
@@ -0,0 +1,62 @@
+const userName = "Yehor";
+// const age = "15";
+
+// const productPrice = "120"
+// const quantity = "3"
+// console.log(userName);
+
+// console.log('hello world' , userName);
+// console.log(userName);
+// console.log(userName.length );
+
+const isOpen = true
+const isOnline = false
+
+// console.log(typeof isOnline)
+
+// console.log("before alert")
+// alert("Привіт");
+// console.log("after alert")
+
+// const answer = confirm("bam boom!!!");
+// console.log(answer)
+
+// let allItems;
+
+// const numbers = prompt("21341");
+// console.log(numbers);
+
+// const letters = prompt("adafafas");
+// console.log(letters);
+
+// const word = prompt("car");
+// console.log(word);
+
+// allItems = numbers + letters + word;
+// console.log(allItems);
+
+// let allNumbers;
+
+// const Num1 = prompt("1");
+// const NumNum1 = Number(Num1)
+// console.log(Num1);
+
+// const Num2 = prompt("2");
+// const NumNum2 = Number(Num2)
+// console.log(Num2);
+
+// const Num3 = prompt("3");
+// const NumNum3 = Number(Num3)
+// console.log(Num3);
+
+// allNumbers = NumNum1 + NumNum2 + NumNum3;
+// console.log(allNumbers);
+
+// const Group = "FE_1y_3_23";
+// console.log("Я навчаюся в GoITeens в групі", Group );
+
+const name = "MacBook Pro 13";
+let price = 10000
+console.log("Обрано", name, "ціна за штуку", price)
+price = 20000
+console.log("Обрано", name, "ціна за штуку", price)
